dbe252d8e4ff9c7ce0047e701057a696581edf24
blackanode <blackanode@gmail.com>
2017-06-27 07:38:03 -0600

    Changed layout used

--- app/src/main/java/baldwin/com/shuffleboard/MainActivity.java
+++ app/src/main/java/baldwin/com/shuffleboard/MainActivity.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.test.suitebuilder.TestMethod;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,17 +14,9 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 
-import org.w3c.dom.Text;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Set;
 import java.util.UUID;
-
-//Todo restore save state
-//Todo disregard bad bluetooth inputs check to see if int
-//Todo add arrays for players 1, and 2 scores
-//Todo make a function that updates the views.
 
 
 public class MainActivity extends AppCompatActivity {
@@ -50,58 +41,65 @@ public class MainActivity extends AppCompatActivity {
     //The sensor number that was red and sent by the bluetooth device
     int sensorNumber;
 
-    //setup Textviews
-    TextView tv_p1_final;
-    TextView tv_p1;
-    TextView tv_p2_final;
-    TextView tv_p2;
-    TextView[] tvPlayer1RoundScores = new TextView[5];
-    TextView[] tvPlayer2RoundScores = new TextView[5];
-    TextView[] tvRoundHeaders = new TextView[6];
+    //setup Textviews Old
+    TextView tvP1Score;
+    TextView tvP1;
+    TextView tvP2Score;
+    TextView tvP2;
+    //TextView[] tvPlayer1RoundScores = new TextView[5];
+    //TextView[] tvPlayer2RoundScores = new TextView[5];
+    //TextView[] tvRoundHeaders = new TextView[6];
     TextView tvShotsTaken;
+    TextView tvRounds;
+    TextView tvShotHistory;
 
-    TextView tvTest;
+    //TextView tvTest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.game);
 
-        tvTest = (TextView) findViewById(R.id.tv_test);
+        //tvTest = (TextView) findViewById(R.id.tv_test);
 
         //Setup player1 scores
-        tv_p1_final = (TextView) findViewById(R.id.tv_p1_final);
-        tv_p1 = (TextView) findViewById(R.id.tv_p1);
-        tv_p1.setBackgroundColor(Color.RED);
+        tvP1Score = (TextView) findViewById(R.id.tv_p1_score);
+        tvP1 = (TextView) findViewById(R.id.tv_p1);
+        tvP1.setBackgroundColor(Color.RED);
 
-        tvPlayer1RoundScores[0] = (TextView) findViewById(R.id.tv_p1_round1);
+        //Setup player2 scores
+        tvP2Score = (TextView) findViewById(R.id.tv_p2_score);
+        tvP2 = (TextView) findViewById(R.id.tv_p2);
+
+        //Shot counter for current player
+        tvShotsTaken = (TextView) findViewById(R.id.tv_shot_counter);
+        //Round counter
+        tvRounds = (TextView) findViewById(R.id.tv_round_counter);
+        //Shot history display
+        tvShotHistory = (TextView) findViewById(R.id.tv_shot_history);
+
+        /*tvPlayer1RoundScores[0] = (TextView) findViewById(R.id.tv_p1_round1);
         tvPlayer1RoundScores[1] = (TextView) findViewById(R.id.tv_p1_round2);
         tvPlayer1RoundScores[2] = (TextView) findViewById(R.id.tv_p1_round3);
         tvPlayer1RoundScores[3] = (TextView) findViewById(R.id.tv_p1_round4);
-        tvPlayer1RoundScores[4] = (TextView) findViewById(R.id.tv_p1_round5);
+        tvPlayer1RoundScores[4] = (TextView) findViewById(R.id.tv_p1_round5);*/
 
-        //Setup player2 scores
-        tv_p2_final = (TextView) findViewById(R.id.tv_p2_final);
-        tv_p2 = (TextView) findViewById(R.id.tv_p2);
-
-        tvPlayer2RoundScores[0] = (TextView) findViewById(R.id.tv_p2_round1);
+        /*tvPlayer2RoundScores[0] = (TextView) findViewById(R.id.tv_p2_round1);
         tvPlayer2RoundScores[1] = (TextView) findViewById(R.id.tv_p2_round2);
         tvPlayer2RoundScores[2] = (TextView) findViewById(R.id.tv_p2_round3);
         tvPlayer2RoundScores[3] = (TextView) findViewById(R.id.tv_p2_round4);
-        tvPlayer2RoundScores[4] = (TextView) findViewById(R.id.tv_p2_round5);
+        tvPlayer2RoundScores[4] = (TextView) findViewById(R.id.tv_p2_round5);*/
 
         //Setup round headers
 
-        tvRoundHeaders[0] = (TextView) findViewById(R.id.tv_round1);
+        /*tvRoundHeaders[0] = (TextView) findViewById(R.id.tv_round1);
         tvRoundHeaders[0].setBackgroundColor(Color.RED);
         tvRoundHeaders[1] = (TextView) findViewById(R.id.tv_round2);
         tvRoundHeaders[2] = (TextView) findViewById(R.id.tv_round3);
         tvRoundHeaders[3] = (TextView) findViewById(R.id.tv_round4);
         tvRoundHeaders[4] = (TextView) findViewById(R.id.tv_round5);
-        tvRoundHeaders[5] = (TextView) findViewById(R.id.tv_final);
-
-        //Shot counter for current player
-        tvShotsTaken = (TextView) findViewById(R.id.tv_shots_taken);
+        tvRoundHeaders[5] = (TextView) findViewById(R.id.tv_final);*/
 
         //Bluetooth adapter
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -169,8 +167,8 @@ public class MainActivity extends AppCompatActivity {
                                 //The variable data now contains our full command
                                 handler.post(new Runnable() {
                                     public void run() {
-                                        //TODO tun updates to views and scores based on data
-                                        tvTest.setText(data);
+
+                                        //tvTest.setText(data);
 
                                         Log.e("workerthread current=", Integer.toString(currentRound));
                                         if (sensorNumber != 5) {
@@ -227,24 +225,24 @@ public class MainActivity extends AppCompatActivity {
 
                                                 if (p1Turn) {
 
-                                                    tv_p1.setBackgroundColor(Color.WHITE);
-                                                    tv_p2.setBackgroundColor(Color.RED);
+                                                    tvP1.setBackgroundColor(Color.WHITE);
+                                                    tvP2.setBackgroundColor(Color.RED);
 
                                                 } else {
                                                     //player 2 turn is over
 
-                                                    tv_p1.setBackgroundColor(Color.RED);
-                                                    tv_p2.setBackgroundColor(Color.WHITE);
-
-                                                    tvRoundHeaders[currentRound].setBackgroundColor(Color.WHITE);
+                                                    tvP1.setBackgroundColor(Color.RED);
+                                                    tvP2.setBackgroundColor(Color.WHITE);
+                                                    tvRounds.setText("Round " + (currentRound + 2) + "/5");
+                                                    /*tvRoundHeaders[currentRound].setBackgroundColor(Color.WHITE);
                                                     tvRoundHeaders[currentRound + 1].setBackgroundColor(Color.RED);
-
-                                                    if(currentRound == 4){
-                                                        tvTest.setText("Game is over");
+                                                    */
+                                                    if (currentRound == 4) {
+                                                        tvShotHistory.setText("Game is over \n" + tvShotHistory.getText());
                                                     }
                                                 }
                                             }
-                                            tvShotsTaken.setText(shotCounter + 0 + "/5");
+                                            tvShotsTaken.setText("Shot " + shotCounter + "/5");
 
                                         }
                                     }
@@ -255,7 +253,6 @@ public class MainActivity extends AppCompatActivity {
 
                         }
                     } catch (IOException e) {
-                        // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
 
@@ -288,11 +285,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }*/
-        (new
-
-                Thread(new workerThread())).
-
-                start();
+        (new Thread(new workerThread())).start();
 
     }//end of onCreate
 
@@ -315,38 +308,39 @@ public class MainActivity extends AppCompatActivity {
                 p2FinalScore = 0;
                 currentRoundScore = 0;
                 shotCounter = 0;
-                tvRoundHeaders[currentRound].setBackgroundColor(Color.WHITE);
-                tvRoundHeaders[0].setBackgroundColor(Color.RED);
-                tv_p1.setBackgroundColor(Color.RED);
-                tv_p2.setBackgroundColor(Color.WHITE);
+                // tvRoundHeaders[currentRound].setBackgroundColor(Color.WHITE);
+                //tvRoundHeaders[0].setBackgroundColor(Color.RED);
+                tvP1.setBackgroundColor(Color.RED);
+                tvP2.setBackgroundColor(Color.WHITE);
                 currentRound = 0;
+                tvRounds.setText("Round 1/5");
                 tvShotsTaken.setText("0/5");
 
-                tvTest.setText("");
+                //tvTest.setText("");
 
                 for (int i = 0; i < 4; i++) {
-                    tvPlayer1RoundScores[i].setText("0");
-                    tvPlayer2RoundScores[i].setText("0");
-                    tv_p1_final.setText("0");
-                    tv_p2_final.setText("0");
+                    //tvPlayer1RoundScores[i].setText("0");
+                    //tvPlayer2RoundScores[i].setText("0");
+                    tvP1Score.setText("0");
+                    tvP2Score.setText("0");
                 }
                 break;
             case R.id.action_next_turn:
                 currentRoundScore = 0;
                 if (p1Turn == true) {
                     p1Turn = false;
-                    tv_p1.setBackgroundColor(Color.WHITE);
-                    tv_p2.setBackgroundColor(Color.RED);
+                    tvP1.setBackgroundColor(Color.WHITE);
+                    tvP2.setBackgroundColor(Color.RED);
                 } else {
                     p1Turn = true;
-                    tv_p2.setBackgroundColor(Color.WHITE);
-                    tv_p1.setBackgroundColor(Color.RED);
-                    tvRoundHeaders[currentRound].setBackgroundColor(Color.WHITE);
+                    tvP2.setBackgroundColor(Color.WHITE);
+                    tvP1.setBackgroundColor(Color.RED);
+                    //tvRoundHeaders[currentRound].setBackgroundColor(Color.WHITE);
                     currentRound += 1;
-                    tvRoundHeaders[currentRound].setBackgroundColor(Color.RED);
+                    //tvRoundHeaders[currentRound].setBackgroundColor(Color.RED);
                     if (currentRound > 4) {
                         //Game is over
-                        tvTest.setText("Game is over");
+                        //tvTest.setText("Game is over");
                     }
                 }
                 break;
@@ -359,28 +353,54 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        
+        //Todo save important variables
+
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        //Todo restore important variables and update views
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
 
     public void calculateScore(int score) {
 
         if (p1Turn) {
             p1FinalScore += score;
-            tv_p1_final.setText(Integer.toString(p1FinalScore));
+            tvP1Score.setText(Integer.toString(p1FinalScore));
             currentRoundScore += score;
             Log.e("calculateScore round =", Integer.toString(currentRound));
-            tvPlayer1RoundScores[currentRound].setText(Integer.toString(currentRoundScore));
+            tvShotHistory.setText("Player 1 scored " + score + " points \n" + tvShotHistory.getText());
+            //tvPlayer1RoundScores[currentRound].setText(Integer.toString(currentRoundScore));
 
         } else {
             p2FinalScore += score;
-            tv_p2_final.setText(Integer.toString(p2FinalScore));
+            tvP2Score.setText(Integer.toString(p2FinalScore));
             currentRoundScore += score;
-            tvPlayer2RoundScores[currentRound].setText(Integer.toString(currentRoundScore));
+            tvShotHistory.setText("Player 2 scored " + score + " points \n" + tvShotHistory.getText());
+            //tvPlayer2RoundScores[currentRound].setText(Integer.toString(currentRoundScore));
         }
 
     }
